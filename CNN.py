9240a11c766aa4e1982dea37f1574acb79f3c897
Austin Ryu <88960218+ryuaus26@users.noreply.github.com>
2023-07-23 18:52:29 -0700

    Update CNN.py

--- CNN.py
+++ CNN.py
@@ -8,7 +8,7 @@ from keras.layers import ReLU, AvgPool2D, Flatten, Dense
 from keras.preprocessing.image import ImageDataGenerator
 from keras import Model
 from keras.callbacks import ModelCheckpoint
-from keras.utils.vis_utils import plot_model
+
 import os
 from tqdm import tqdm
 import numpy as np
@@ -52,51 +52,51 @@ val_ds  = tf.keras.utils.image_dataset_from_directory (
     shuffle=True,
     
 )
-print(val_ds)
-# def mobilnet_block(x, filters, strides):
-#     x = DepthwiseConv2D(kernel_size=3, padding='same', strides=strides)(x)
-#     x = BatchNormalization()(x)
-#     x = ReLU()(x)
+
+def mobilnet_block(x, filters, strides):
+    x = DepthwiseConv2D(kernel_size=3, padding='same', strides=strides)(x)
+    x = BatchNormalization()(x)
+    x = ReLU()(x)
     
-#     x = Conv2D(filters=filters, kernel_size=1, strides=strides, padding='same')(x)
-#     x = BatchNormalization()(x)
-#     x = ReLU()(x)
+    x = Conv2D(filters=filters, kernel_size=1, strides=strides, padding='same')(x)
+    x = BatchNormalization()(x)
+    x = ReLU()(x)
     
-#     return x
+    return x
     
-# input = Input(shape=(224, 224, 3))
-# x = Conv2D(filters=32, kernel_size=3, strides=1, padding='same')(input)
-# x = BatchNormalization()(x)
-# x = ReLU()(x)
+input = Input(shape=(224, 224, 3))
+x = Conv2D(filters=32, kernel_size=3, strides=1, padding='same')(input)
+x = BatchNormalization()(x)
+x = ReLU()(x)
 
-# # main part of the model
-# x = mobilnet_block(x, filters=64, strides=1)
-# x = mobilnet_block(x, filters=128, strides=1)
-# x = mobilnet_block(x, filters=128, strides=2)
-# x = mobilnet_block(x, filters=256, strides=1)
-# x = mobilnet_block(x, filters=256, strides=2)
-# x = mobilnet_block(x, filters=512, strides=1)
+# main part of the model
+x = mobilnet_block(x, filters=64, strides=1)
+x = mobilnet_block(x, filters=128, strides=1)
+x = mobilnet_block(x, filters=128, strides=2)
+x = mobilnet_block(x, filters=256, strides=1)
+x = mobilnet_block(x, filters=256, strides=2)
+x = mobilnet_block(x, filters=512, strides=1)
 
-# for _ in range(5):
-#     x = mobilnet_block(x, filters=512, strides=1)
-# x = mobilnet_block(x, filters=1024, strides=1)
-# x = mobilnet_block(x, filters=1024, strides=1)
+for _ in range(5):
+    x = mobilnet_block(x, filters=512, strides=1)
+x = mobilnet_block(x, filters=1024, strides=1)
+x = mobilnet_block(x, filters=1024, strides=1)
 
-# x = AvgPool2D(pool_size=7, strides=1, data_format='channels_first')(x)
-# x = Flatten()(x)
-# output = Dense(units=1, activation='sigmoid')(x)
+x = AvgPool2D(pool_size=7, strides=1, data_format='channels_first')(x)
+x = Flatten()(x)
+output = Dense(units=1, activation='sigmoid')(x)
 
-# model = Model(inputs=input, outputs=output)
+model = Model(inputs=input, outputs=output)
 
-# checkpoint = ModelCheckpoint(
-#     './Weights/weights.h5',  # Path to save the weights file
-#     monitor='val_loss',  # Metric to monitor for saving the weights
-#     save_best_only=True,  # Save only the best weights
-#     save_weights_only=True  # Save only the weights, not the entire model
-# )
+checkpoint = ModelCheckpoint(
+    './Weights/weights.h5',  # Path to save the weights file
+    monitor='val_loss',  # Metric to monitor for saving the weights
+    save_best_only=True,  # Save only the best weights
+    save_weights_only=True  # Save only the weights, not the entire model
+)
 
-# model.summary()
+model.summary()
 
-# model.compile(optimizer='adam', loss=tf.keras.losses.BinaryCrossentropy(), metrics=['accuracy'])
-# model.fit(train_ds, validation_data=val_ds, callbacks=checkpoint, epochs=7, batch_size=batch_size,workers=30)
-# model.save('FaceDetectModel.h5')
+model.compile(optimizer='adam', loss=tf.keras.losses.BinaryCrossentropy(), metrics=['accuracy'])
+model.fit(train_ds, validation_data=val_ds, callbacks=checkpoint, epochs=7, batch_size=batch_size,workers=30)
+model.save('FaceDetectModel.h5')
