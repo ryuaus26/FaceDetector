9207d2ebc1d452aea7b80c0d06e0f3508413aebd
Austin Ryu <88960218+ryuaus26@users.noreply.github.com>
2023-07-23 18:52:04 -0700

    Update test.py

--- test.py
+++ test.py
@@ -2,10 +2,10 @@ import tensorflow as tf
 from keras.models import load_model
 import keras.utils as image
 import numpy as np
-from CNN import model
 
-test_model = load_model('model.h5')
-img_path = '/Users/ryuaus26/Desktop/Screen Shot 2023-06-28 at 8.14.28 PM.png'  # Replace with the path to your input image file
+
+test_model = load_model('FaceWeights.h5')
+img_path = ''  # Replace with the path to your input image file
 img = image.load_img(img_path, target_size=(224, 224))  # Set the target size according to your model's input shape
 x = image.img_to_array(img)
 x = np.expand_dims(x, axis=0)
